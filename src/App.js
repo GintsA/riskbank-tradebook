d4c065168d3d9cbfbcbe3b0df75922aa234accf0
gintsakselis <gintsakselis2@inbox.lv>
2025-07-06 01:21:34 +0300

    Adding Clock component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Clock from './Clock';
 
 function App() {
   return (
     <div className="App">
       <h1 className="title">Project - RiskBank TradeBook</h1>
+      <Clock />
     </div>
   );
 }
